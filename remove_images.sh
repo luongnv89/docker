5f991860df6ca4582fbae4fd62a153f9b1aadcc4
NGUYEN VAN LUONG <luongnv89@gmail.com>
2015-06-24 13:49:38 +0200

    deleted:    remove_containers.sh
    	.Dockerfile.swp

--- remove_images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Remove all containers
-
-echo "Operator detected: $OSTYPE"
-if [[ "$OSTYPE"=="darwin"* ]]; then
-	docker rmi $(docker images -a -q)
-else
-	sudo docker rmi $(sudo docker images -a -q)
-fi
